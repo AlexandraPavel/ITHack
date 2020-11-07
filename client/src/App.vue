1f11e19df1e5529c8af757ecf99705b08885ac8a
Alexandru Robert <robertalexandru2712@gmail.com>
2020-11-07 07:22:29 +0200

    Before adding vuetify

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id='wrapper-app'>
+        <h1>Nunito</h1>
+    </div>
+</template>
+
+<script lang='ts'>
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+
+
+    @Component({})
+    export default class App extends Vue {
+       
+    }
+</script>
+
+<style lang='scss' scoped>
+    
+</style>
